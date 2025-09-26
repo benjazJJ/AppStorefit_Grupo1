088954ba7981ea678516fe06598da29b76a65270
SSDD <be.palmap@duocuc.cl>
2025-09-26 12:12:27 -0300

    Avances en Screen Expandida

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenExpandida.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenExpandida.kt
@@ -1,9 +1,12 @@
 package com.example.appstorefit_grupo1.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,8 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.appstorefit_grupo1.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,7 +49,16 @@ fun HomeScreenExpandida(){
                     Text("Presioname...")
                 }
             }
-            Column(modifier = Modifier.weight(1f)) {  }
+            Column(modifier = Modifier.weight(1f)) {
+                Image(
+                    painter = painterResource(R.drawable.gatito),
+                    contentDescription = "Logo App",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
