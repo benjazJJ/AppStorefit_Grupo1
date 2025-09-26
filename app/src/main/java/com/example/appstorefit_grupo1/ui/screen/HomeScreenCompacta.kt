6bb9446b81724600a1d229c23c0afbf1f7c6260d
SSDD <be.palmap@duocuc.cl>
2025-09-26 11:58:57 -0300

    Primera Screen terminada

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
@@ -1,9 +1,13 @@
 package com.example.appstorefit_grupo1.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,8 +15,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.appstorefit_grupo1.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,6 +42,17 @@ fun HomeScreenCompacta(){
                 "Bienvenido",
                 color = MaterialTheme.colorScheme.error,
                 style = MaterialTheme.typography.titleLarge
+            )
+            Button(onClick = {}) {
+                Text("Presioname...")
+            }
+            Image(
+                painter = painterResource(R.drawable.gatito),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
             )
         }
     }
