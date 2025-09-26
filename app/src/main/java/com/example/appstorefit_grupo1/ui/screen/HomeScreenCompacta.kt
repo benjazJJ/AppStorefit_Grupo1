1168eeff152aa62dc5ac5f5b9cd7db2e1b9b157f
SSDD <be.palmap@duocuc.cl>
2025-09-26 11:47:23 -0300

    Scaffold

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
@@ -1,13 +1,36 @@
 package com.example.appstorefit_grupo1.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenCompacta(){
 
+    Scaffold( //Siempre debemos construir el Scaffold primero
+        topBar = {
+            TopAppBar({Text("Hola Mundo")})
+
+        }
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) { }
+    }
 }
 
 
