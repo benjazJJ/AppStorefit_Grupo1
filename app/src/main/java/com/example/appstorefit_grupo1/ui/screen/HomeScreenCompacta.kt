e3646478a18a477fc24b81805d12b1ee10218b98
SSDD <be.palmap@duocuc.cl>
2025-09-26 11:49:01 -0300

    Scaffold

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -29,7 +30,13 @@ fun HomeScreenCompacta(){
                 .fillMaxSize()
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
-        ) { }
+        ) {
+            Text(
+                "Bienvenido",
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
     }
 }
 
