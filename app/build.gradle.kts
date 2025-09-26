12cc956c95e12a1b5de08c4a3ef75567bf4bb7d2
SSDD <be.palmap@duocuc.cl>
2025-09-26 12:20:23 -0300

    Añadimos dependencia de window size class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //Implementación de la libreria de window Size Class
+    implementation("androidx.compose.material3:material3-window-size-class")
 }
