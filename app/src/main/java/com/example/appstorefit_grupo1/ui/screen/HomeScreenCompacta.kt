3af68796e2aab2540f31720ef4686be089759c50
SSDD <be.palmap@duocuc.cl>
2025-09-26 11:33:52 -0300

    Creamos funciones

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreenCompacta.kt
@@ -1,2 +1,20 @@
 package com.example.appstorefit_grupo1.ui.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenCompacta(){
+
+}
+
+
+
+
+@Preview(name = "Compacta", widthDp = 320, heightDp = 800)
+@Composable
+fun HomeScreenCompactaPreview(){
+
+}
