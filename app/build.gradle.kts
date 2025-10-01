d2f8f2cac2fc9a0a1cb823d7e44b040a6c0e55b6
SSDD <gust.espinoza@duocuc.cl>
2025-10-01 10:38:24 -0300

    Creacion nuveos package, navegation y components y nuevas librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,9 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     //Implementación de la libreria de window Size Class
     implementation("androidx.compose.material3:material3-window-size-class")
+    // librerias nuevas,
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }
