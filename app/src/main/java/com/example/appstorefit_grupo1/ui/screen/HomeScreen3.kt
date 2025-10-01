fff3da20bf230e1a34dbab0aace85983a857e036
SSDD <gust.espinoza@duocuc.cl>
2025-10-01 11:11:38 -0300

    Creacion de HomeScreen3 y funcion con Unit

--- app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreen3.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/HomeScreen3.kt
@@ -1,6 +1,22 @@
 package com.example.appstorefit_grupo1.ui.screen
 
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -11,6 +27,28 @@ fun HomeScreen3(
 
 ){
 
+    var bg = MaterialTheme.colorScheme.surfaceVariant
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .background(bg)
 
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    "Home",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(Modifier.width(8.dp))
+                AssistChip(
+                    onClick = {},
+                    label = {Text("Click here")}
+                )
+            }
+        }
+    }
 
 }
