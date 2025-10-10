5a112f984578708a81f51ddf0cd6866007d28741
SSDD <gust.espinoza@duocuc.cl>
2025-10-10 12:24:13 -0300

    Cree LoginScreen y NavGraph

--- app/src/main/java/com/example/appstorefit_grupo1/ViewModel/AuthViewModel.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/ViewModel/AuthViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.appstorefit_grupo1.ViewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appstorefit_grupo1.domain.validation.validateEmail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 //Estructuras de datos para los formularios(pantallas)
 
@@ -16,7 +23,7 @@ data class LoginuiState(
 
     // varables del formulario general
     val msgError: String? = null,
-    val isSubmitting: Boolean? = false, // flag carga
+    val isSubmitting: Boolean = false, // flag carga
     val canSubmit: Boolean = false, // habilitar boton
     val succes: Boolean = false //resltado ok
 )
@@ -39,7 +46,7 @@ data class RegisterUiState(
 
     //variables del formulario
     val msgError: String? = null,
-    val isSubmitting: Boolean? = false, // flag carga
+    val isSubmitting: Boolean = false, // flag carga
     val canSubmit: Boolean = false, // habilitar boton
     val succes: Boolean = false //resltado ok
 )
@@ -56,6 +63,60 @@ data class DemoUser(
 
 class AuthViewModel: ViewModel(){
 
+    //crear coneccion etatica e datos compartida
+
+    companion object{
+        //creo la coleccion
+        private val USERS = mutableListOf(
+            //Usuario de prueba
+            DemoUser("Juan","a@a.cl","12345678","Juan123!")
+        )
+
+    }
+    private val _login = MutableStateFlow(LoginuiState())
+    val login: StateFlow<LoginuiState> = _login
+
+    private val _register = MutableStateFlow(RegisterUiState())
+    val register: StateFlow<RegisterUiState> = _register
+
+    //manipuladores el login
+
+    fun recomputeLoginCanSubmit(){
+        val s = _login.value
+        val can = s.emailError != null && s.emailError.isNotBlank() && s.pass.isNotBlank()
+        _login.update { it.copy(canSubmit = can) }
+    }
+
+    fun onLoginEmailChangue(value: String){
+        _login.update { it.copy(email = value, emailError = validateEmail(value)) }
+    }
+
+    fun onLoginPassChangue(value: String){
+        _login.update { it.copy(pass = value) }
+        recomputeLoginCanSubmit()
+    }
+
+    fun submitLogin(){
+        val s = _login.value
+        if(!s.canSubmit || s.isSubmitting) return
+        viewModelScope.launch {
+            _login.update { it.copy(isSubmitting = true, msgError = null, succes = false) }
+            delay(1000)
+            //buscamos si el usuario existe
+            val user = USERS.firstOrNull{it.correo.equals(s.email, ignoreCase = true)}
+            val ok = user != null && user.pass == s.pass
+
+            _login.update { it.copy(
+                isSubmitting = false,
+                succes = ok,
+                msgError = if(!ok) "Credeciales invalidas" else null
+            ) }
+        }
+    }
+
+    fun cleraLoginResult(){
+        _login.update { it.copy(succes = false, msgError = null) }
+    }
 
 
 }
---
+++ app/src/main/java/com/example/appstorefit_grupo1/ui/screen/LoginScreen.kt
@@ -0,0 +1,176 @@
+package com.example.appstorefit_grupo1.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.appstorefit_grupo1.ViewModel.AuthViewModel
+
+@Composable
+fun LoginScreenVm(
+    onLoginOkNavigateHome: () -> Unit,   // Acción para “ir” a Home Login ok
+    onGoRegister: () -> Unit // Acción para ir a Registro
+){
+    //variable de instancia del viewmodel
+    val vm: AuthViewModel = viewModel()
+    val state by vm.login.collectAsStateWithLifecycle()
+
+    if(state.succes){
+        //redirecciono al home
+        vm.cleraLoginResult()
+        onLoginOkNavigateHome()
+    }
+
+    LoginScreen(
+        email = state.email,
+        pass = state.pass,
+        emailError = state.emailError,
+        passError = state.passError,
+        canSubmit = state.canSubmit,
+        isSubmitting = state.isSubmitting,
+        erorMsg = state.msgError,
+        onEmailChange = vm::onLoginEmailChangue,
+        onPassChange = vm::onLoginPassChangue,
+        onSubmit = vm::submitLogin,
+        onGoRegister = onGoRegister
+    )
+
+
+}
+@Composable // Pantalla Login (solo navegación, sin formularios)
+fun LoginScreen(
+
+    email: String,
+    pass: String,
+    emailError: String?,
+    passError: String?,
+    canSubmit: Boolean,
+    isSubmitting: Boolean,
+    erorMsg: String?,
+    onEmailChange: (String) -> Unit,
+    onPassChange: (String) -> Unit,
+    onSubmit: () -> Unit,
+    onGoRegister: () -> Unit
+
+) {
+    val bg = MaterialTheme.colorScheme.secondaryContainer // Fondo distinto para contraste
+    var showPass by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize() // Ocupa todo
+            .background(bg) // Fondo
+            .padding(16.dp), // Margen
+        contentAlignment = Alignment.Center // Centro
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally // Centrado horizontal
+        ) {
+            Text(
+                text = "Login",
+                style = MaterialTheme.typography.headlineSmall // Título
+            )
+            Spacer(Modifier.height(12.dp)) // Separación
+            Text(
+                text = "Pantalla de Login (demo). Usa la barra superior, el menú lateral o los botones.",
+                textAlign = TextAlign.Center // Alineación centrada
+            )
+            Spacer(Modifier.height(20.dp)) // Separación
+
+            //borre lo anterior y dibujo mi formulario
+            // -- Email --
+            OutlinedTextField(
+                value = email,
+                onValueChange = onEmailChange,
+                label = { Text("Correo Electrónico")},
+                singleLine = true,
+                isError = emailError != null,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            if(emailError != null){
+                Text(emailError, color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelSmall)
+            }
+            Spacer(Modifier.height(8.dp))
+
+            // -- Pass --
+            OutlinedTextField(
+                value = pass,
+                onValueChange = onPassChange,
+                label = { Text("Contraseña")},
+                singleLine = true,
+                isError = passError != null,
+                visualTransformation = if(showPass) VisualTransformation.None
+                else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(onClick = { showPass = !showPass }) {
+                        Icon(
+                            imageVector = if(showPass) Icons.Filled.VisibilityOff
+                            else Icons.Filled.Visibility,
+                            contentDescription = if(showPass) "Ocultar Contraseña"
+                            else "Mostrar Contraseña"
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            if(passError != null){
+                Text(passError, color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelSmall)
+            }
+            Spacer(Modifier.height(12.dp))
+
+            Button(
+                onClick = onSubmit,
+                enabled = canSubmit && !isSubmitting,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                if(isSubmitting){
+                    CircularProgressIndicator(strokeWidth = 2.dp, modifier = Modifier.size(18.dp))
+                    Spacer(Modifier.width(8.dp))
+                    Text("Validando ...")
+                }
+                else{
+                    Text("Iniciar Sesión")
+                }
+            }
+            if(erorMsg != null){
+                Text(erorMsg, color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelSmall)
+            }
+            Spacer(Modifier.height(10.dp))
+            OutlinedButton(
+                onClick = onGoRegister,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Crear Cuenta")
+            }
+
+        }
+    }
+}
