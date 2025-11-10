719362181ac5d043bdb44eee96bf029ad30db9dc
Benjamín <be.palmap@duocuc.cl>
2025-11-10 19:21:49 -0300

    CRUD administrador casi funcionando, terminar el que aparezca en productos

--- app/src/main/java/com/example/appstorefit_grupo1/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/appstorefit_grupo1/data/local/database/AppDatabase.kt
@@ -41,7 +41,7 @@ import com.example.appstorefit_grupo1.data.local.Mensaje.MensajeEntity
         CompraEntity::class,
         CompraDetalleEntity::class
     ],
-    version = 38,
+    version = 39,
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
